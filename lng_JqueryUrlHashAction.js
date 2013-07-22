8e16b15cd4eba39fc79f4145ab3ed11f346945b2
Laurent3105 <laurent.community@gmail.com>
2013-07-22 19:59:43 +0200

    stabilisation du blugin

--- lng_JqueryUrlHashAction.js
+++ lng_JqueryUrlHashAction.js
@@ -1,9 +1,37 @@
+/*
+ * 
+ */
 (function($) {
-	// définition du plugin
-	$.fn.lng_UrlHashAction = function() {
-		alert("test ok");
+	var lng_UrlHashAction_title = document.title;
+	if(location.hash !== "")
+	document.title = lng_UrlHashAction_title + ' | ' + ( location.hash.replace( /^#/, '' ) || 'blank' );
+	$.fn.lng_UrlHashAction = function(options) {
+		var defaults = {
+			callback : null
+		};
+		var opts = $.extend(defaults, options);
+
+		function createAction() {
+			$(this).find("li > a").each(function(){
+				if($(this).attr("lng_action") !== ""){
+					$(this).click(onClick);
+				}
+			});
+		}
+
+		function setTitle() {
+			
+		}
+		
+		function onClick(){
+			document.title = lng_UrlHashAction_title + ' | ' + $(this).attr("lng_title");
+			opts.callback();
+		}
+		
+
+		// création d'une liste d'action
+		$(this).each(createAction);
+		// interface fluide
 		return $(this);
 	};
-
-	
 })(jQuery);
